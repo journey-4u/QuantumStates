b0694aa79c9f2c646b69539f598e7e04e69f6d01
journey-4u <48493671+journey-4u@users.noreply.github.com>
2019-11-11 16:04:45 +0000

    Added S+ and S- operators

--- src/com/company/TwoStateSystem.java
+++ src/com/company/TwoStateSystem.java
@@ -28,6 +28,18 @@ public class TwoStateSystem {
         return this.c1.ModulusSq();
     }
 
+    public static TwoStateSystem Add(TwoStateSystem system1, TwoStateSystem system2)
+    {
+        TwoStateSystem ResultantSystem = new TwoStateSystem(Complex.Add(system1.c0,system2.c0),Complex.Add(system1.c1,system2.c1));
+        return ResultantSystem;
+    }
+
+    public static TwoStateSystem Multiply(Complex Z, TwoStateSystem system)
+    {
+        TwoStateSystem ResultantSystem = new TwoStateSystem(Complex.Multiply(Z,system.c0),Complex.Multiply(Z,system.c1));
+        return ResultantSystem;
+    }
+
     public String toString(){
 
         String SystemString = new String();
