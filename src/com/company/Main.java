10d9fa9d9be5c1fa79f4b413605713ccbea067b4
journey-4u <48493671+journey-4u@users.noreply.github.com>
2019-11-11 13:24:33 +0000

    Added operators for the Spin in the X and Y directions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,20 +13,28 @@ public class Main {
         return System;
     }
 
+    public static SpinHalfSystem Sx(SpinHalfSystem System){
+
+        System = new SpinHalfSystem(Complex.Multiply(0.5,System.c1),Complex.Multiply(0.5,System.c0));
+        return System;
+    }
+
+    public static SpinHalfSystem Sy(SpinHalfSystem System){
+
+        System = new SpinHalfSystem(Complex.Multiply(new Complex(0,-0.5),System.c1),Complex.Multiply(new Complex(0,0.5),System.c0));
+        return System;
+    }
+
     public static void main(String[] args) {
 
-	    Complex Z = new Complex(2,1);
-        //Complex.printZ(Z);
-        Complex Z2 = Complex.Add(Z,new Complex(2,-5));
-        //.printZ(Z2);
+        Complex Z = new Complex(0.0710678118,0.70710678118);
 
-        SpinHalfSystem Phi = new SpinHalfSystem(Z,Z2);
+        SpinHalfSystem Electron = new SpinHalfSystem(Z,Z);
 
-        System.out.println(Phi.toString());
-        System.out.println(Sz(Phi).toString());
+        System.out.println(Electron);
+        System.out.println(Sz(Electron));
+        System.out.println(Sx(Electron));
+        System.out.println(Sy(Electron));
 
-        // hbar \u0127
-        // Down Arrow, \u2191
-        // Down Arrow, \u2193
     }
 }
