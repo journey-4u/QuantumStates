3e6fbea4d4189ec0072f95d01b931070e45ec119
journey-4u <48493671+journey-4u@users.noreply.github.com>
2019-11-11 16:05:53 +0000

    Added S+ and S- operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import javafx.scene.transform.MatrixType;
+
+import javax.sound.midi.Soundbank;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Main {
 
     public static TwoStateSystem Hamiltonian(TwoStateSystem System){
@@ -25,16 +30,41 @@ public class Main {
         return System;
     }
 
+    public static SpinHalfSystem SPlus(SpinHalfSystem System){
+
+        SpinHalfSystem Result = new SpinHalfSystem();
+        Result =  SpinHalfSystem.Add(Sx(System), SpinHalfSystem.Multiply(new Complex(0,1),Sy(System)) )   ;
+        return Result;
+    }
+
+    public static SpinHalfSystem SMinus(SpinHalfSystem System){
+
+        SpinHalfSystem Result = new SpinHalfSystem();
+        Result =  SpinHalfSystem.Add(Sx(System), SpinHalfSystem.Multiply(new Complex(0,-1),Sy(System)) )   ;
+        return Result;
+    }
+
+
+
     public static void main(String[] args) {
 
         Complex Z = new Complex(0.0710678118,0.70710678118);
 
         SpinHalfSystem Electron = new SpinHalfSystem(Z,Z);
 
-        System.out.println(Electron);
-        System.out.println(Sz(Electron));
-        System.out.println(Sx(Electron));
-        System.out.println(Sy(Electron));
+        SpinHalfSystem Up = new SpinHalfSystem(new Complex(1,0), new Complex(0,0));
+        SpinHalfSystem Down = new SpinHalfSystem(new Complex(0,0), new Complex(1,0));
+
+
+        System.out.println("__________________________________________________________________________");
+
+        System.out.println(SMinus(Up));
+        System.out.println(SMinus(Down));
+
+
+
+
+
 
     }
 }
