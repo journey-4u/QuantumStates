51c986e15d36db1ea822b48edf87a99a7eb91a0a
journey-4u <48493671+journey-4u@users.noreply.github.com>
2019-11-12 16:36:54 +0000

    Update Main.java
    Practiced with Time Evolution Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ import javafx.scene.transform.MatrixType;
 
 import javax.sound.midi.Soundbank;
 import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class Main {
 
@@ -44,25 +47,76 @@ public class Main {
         return Result;
     }
 
+    public static SpinHalfSystem simpleHamiltonian(SpinHalfSystem system){
+        double hbar = 0.00000000000000000000000000000000010545718;
+
+        Complex upCo = Complex.Multiply(0.5*hbar,system.c1);
+        Complex downCo = Complex.Multiply(0.5*hbar,system.c0);
 
 
-    public static void main(String[] args) {
 
-        Complex Z = new Complex(0.0710678118,0.70710678118);
+        SpinHalfSystem ResultantSystem = new SpinHalfSystem(upCo,downCo);
 
-        SpinHalfSystem Electron = new SpinHalfSystem(Z,Z);
+        return ResultantSystem;
 
-        SpinHalfSystem Up = new SpinHalfSystem(new Complex(1,0), new Complex(0,0));
-        SpinHalfSystem Down = new SpinHalfSystem(new Complex(0,0), new Complex(1,0));
+    }
+
+    public static SpinHalfSystem simpleTimeEvolution(SpinHalfSystem system,double t){
+        double hbar = 0.00000000000000000000000000000000010545718;
+
+        SpinHalfSystem ZerothTerm = system;
+        SpinHalfSystem FirstTerm =   SpinHalfSystem.Multiply(new Complex(0,-(t/hbar)), simpleHamiltonian(system));
+        SpinHalfSystem SecondTerm =   SpinHalfSystem.Multiply(new Complex(-((t*t)/(hbar*hbar)),0), simpleHamiltonian(simpleHamiltonian(system)));
+        SpinHalfSystem ThirdTerm =   SpinHalfSystem.Multiply(new Complex(0,((t*t*t)/(hbar*hbar*hbar))), simpleHamiltonian(simpleHamiltonian(simpleHamiltonian(system))));
 
 
+        SpinHalfSystem ResultantSystem = ZerothTerm;
+        ResultantSystem = SpinHalfSystem.Add(ResultantSystem,FirstTerm);
+        ResultantSystem = SpinHalfSystem.Add(ResultantSystem,SecondTerm);
+        ResultantSystem = SpinHalfSystem.Add(ResultantSystem,ThirdTerm);
+
+        return ResultantSystem;
+        // Results are more accurate the less time that has progressesd
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+
+        Complex Z = new Complex(0.70710678118,0);
+
+        SpinHalfSystem upElectron = new SpinHalfSystem(new Complex(1,0),new Complex(0,0));
+        SpinHalfSystem downElectron = new SpinHalfSystem(new Complex(0,0),new Complex(1,0));
+
+        SpinHalfSystem upElectron2 = new SpinHalfSystem(new Complex(0,1),new Complex(0,0));
+        SpinHalfSystem downElectron2 = new SpinHalfSystem(new Complex(0,0),new Complex(0,1));
+
+        SpinHalfSystem Electron = new SpinHalfSystem(new Complex(0.5,0),new Complex(0.866,0));
+
+        System.out.println(upElectron);
+        System.out.println(downElectron);
+        System.out.println(upElectron2);
+        System.out.println(downElectron2);
+        System.out.println(Electron);
+        System.out.println(System.getProperty("user.dir"));
+
+        double Time = 4;
+        int precision = 4;
+        double deltaT = Time / Math.pow(10,precision);
+        var N = (int) Math.pow(10,precision);
+        SpinHalfSystem altState = simpleTimeEvolution(Electron,deltaT);
+        PrintWriter writer = new PrintWriter("D:/the-file-name.txt", "UTF-8");
+        for(int i = 0; i <N ;i++)
+        {
+            altState = simpleTimeEvolution(altState,deltaT);
+            writer.println(altState);
+        }
+        System.out.println(altState);
+        SpinHalfSystem finalState = altState;
+        System.out.println(Electron);
+        System.out.println(finalState);
         System.out.println("__________________________________________________________________________");
-
-        System.out.println(SMinus(Up));
-        System.out.println(SMinus(Down));
-
-
-
+        System.out.println(Electron.State0Probability() + "     :     "+ Electron.State1Probability());
+        System.out.println(finalState.State0Probability() + "     :     "+ finalState.State1Probability());
+        System.out.println("__________________________________________________________________________");
 
 
 
